710e1ed20fbc207c04255d363409d7e0b380920c
Hamza Tariq <hamzatrq116@gmail.com>
2022-04-05 22:16:43 +0500

    Adding support for weba-wallet

--- example/src/app.js
+++ example/src/app.js
@@ -1,15 +1,14 @@
-import 'regenerator-runtime/runtime'
-import { MetamaskManager } from '../../dist'
+import { MetamaskManager } from '../../dist';
 
-
+let wallet = undefined; // iframe element
 
 async function login(type) {
     if (type === 'metamask') {
         const metamask = new MetamaskManager();
         try {
             const signedMessage = await metamask.connect();
-            await metamask.login(signedMessage);
-            console.log(await metamask.nft.getNFTs());
+            const jwt = await metamask.login(signedMessage);
+            sendMessage('initiate_wallet', { jwt })
         } catch (error) {
             alert(error.message);
         }
@@ -18,11 +17,65 @@ async function login(type) {
     }
 }
 
+async function getProfile() {
+    console.log('get profile')
+    sendMessage('get_profile', {});
+}
+
+async function setProfile(key, value) {
+    console.log('set profile')
+    sendMessage('set_profile', { key, value });
+}
+
+function sendMessage(method, data = {}) {
+    const message = {
+        method,
+        data
+    }
+    wallet.contentWindow.postMessage(JSON.stringify(message), "*");
+}
+
+function receiveMessage(event) {
+    console.log('receiveMessage')
+    if (event.origin + '/' !== wallet.src) { // Adding '/' at the end to compare origin and src
+        return;
+    }
+    if (!event.data) {
+        return;
+    }
+    const res = JSON.parse(event.data);
+    const { type, method, data, error } = res;
+    if (error) {
+        window.alert(error);
+        return;
+    }
+    if (type === 'event') {
+        console.log(`Got event: ${method}`, { data })
+    } else if (type === 'response') {
+        console.log(`Got response for: ${method}`, { data })
+    }
+}
+
 window.onload = () => {
+    wallet = document.getElementById('wallet');
+    window.addEventListener("message", receiveMessage, false);
     document.getElementById('loginMetamask').addEventListener('click', () => {
         login('metamask');
     });
     document.getElementById('loginDiscord').addEventListener('click', () => {
         login('discord');
     });
+    document.getElementById('btn-profile').addEventListener('click', () => {
+        getProfile();
+    });
+
+    document.getElementById('setName').addEventListener('click', () => {
+        const name = document.getElementById('nameInput').value;
+        if (!name.trim()) {
+            alert('Invalid value for name');
+        } else {
+            setProfile('name', name);
+        }
+    });
+
 }
